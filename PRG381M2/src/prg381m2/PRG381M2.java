d72336f40201d8b80d77c2b68a5cf8b79d9fc492
Jana <156057081+GetafixPotion@users.noreply.github.com>
2025-07-15 12:22:11 +0200

    Add files via upload
    Testing the commit and push for M2

---
+++ PRG381M2/src/prg381m2/PRG381M2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prg381m2;
+
+/**
+ *
+ * @author hdesign
+ */
+public class PRG381M2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Testing if this will commit and push to repo
+    }
+    
+}
