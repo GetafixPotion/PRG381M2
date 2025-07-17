61bb01bf3f3d49aad2e32803a529309d249cc795
Jana <janarademan1236@gmail.com>
2025-07-17 18:14:32 +0200

    added my comments

--- PRG381M2/src/prg381m2/DBConnection.java
+++ PRG381M2/src/prg381m2/DBConnection.java
@@ -19,14 +19,14 @@ import java.sql.PreparedStatement;
  */
 public class DBConnection {
      
-    private static final String DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
-    private static final String JDBC_URL = "jdbc:derby:counselorDB;create=true";
-    Connection con;
+    private static final String DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";//JDBC driver vir embedded derby
+    private static final String JDBC_URL = "jdbc:derby:counselorDB;create=true"; //this is the embedded url to connect and create db
+    Connection con; //live db connection object
 
     public void connect() throws ClassNotFoundException {
         try {
-            Class.forName(DRIVER);
-            this.con = DriverManager.getConnection(JDBC_URL);
+            Class.forName(DRIVER); //loads the driver
+            this.con = DriverManager.getConnection(JDBC_URL);//connects the DB
             if (this.con != null) {
                 System.out.println("Connected to database");
             }
@@ -54,7 +54,7 @@ public class DBConnection {
         try {
             String query = "SELECT * FROM Counselor";
             ResultSet rs = this.con.createStatement().executeQuery(query);
-
+ //this loops through ResultSet om data te collect
             while (rs.next()) {
                 String name = rs.getString("NameCounselor");
                 String spec = rs.getString("Specialization");
@@ -64,13 +64,14 @@ public class DBConnection {
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
+         //returns a list of string arrays for each counslor row
         return dataList;
     }
 
     public void add(String name, String specialization, String availability) {
         try {
             String query = "INSERT INTO Counselor (NameCounselor, Specialization, Availibility) VALUES (?, ?, ?)";
-            PreparedStatement ps = this.con.prepareStatement(query);
+            PreparedStatement ps = this.con.prepareStatement(query);//this is to avoid SQL injection/security vulnerability (a PreparedStatement is a safe way to write SQL queries in Java that separates the SQL logic from the user input.)
             ps.setString(1, name);
             ps.setString(2, specialization);
             ps.setString(3, availability);
@@ -84,8 +85,8 @@ public class DBConnection {
         try {
             String query = "DELETE FROM Counselor WHERE NameCounselor = ?";
             PreparedStatement ps = this.con.prepareStatement(query);
-            ps.setString(1, name);
-            ps.executeUpdate();
+            ps.setString(1, name);//insert the value stored in the variable name into the first placeholder of SQL q
+            ps.executeUpdate(); //executes the SQL statem,ents
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
@@ -100,7 +101,7 @@ public class DBConnection {
             ps.setString(3, name);
             ps.executeUpdate();
         } catch (SQLException ex) {
-            ex.printStackTrace();
+            ex.printStackTrace();//exception object: prints a detailed error report
         }
     }
 }
